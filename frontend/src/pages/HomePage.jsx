c47baa6f58a64b1c2061ec8f137b269c8089dd1c
SGMohan <iamsgmohan@gmail.com>
2026-03-24 12:56:43 +0530

    Corrected

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import { useContext, useState } from 'react'
 import Sidebar from '../components/Sidebar';
 import ChatContainer from '../components/ChatContainer';
-import RightSideBar from '../components/RightSideBar';
+import RightSidebar from '../components/RightSidebar';
 import { ChatContext } from '../context/ChatContext';
 
 const HomePage = () => {
@@ -16,7 +16,7 @@ const HomePage = () => {
           h-full grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2' }`}>
               <Sidebar  />
               <ChatContainer />
-              <RightSideBar />
+              <RightSidebar />
           </div>
           
     </div>
